85761d950ce298da6530cb911967b877b7355139
biribbo <ratajczakbarbara2208@gmail.com>
2025-03-07 17:02:03 +0100

    wrong file attached

--- zad1.py
+++ zad1.py
@@ -1,25 +1,28 @@
 import numpy as np
 from itertools import product
 
-def NOT(u1):
-    weights = np.array([-1.5, 1])
-    inputs = np.array([u1, 1])
+def step_function(weights, inputs):
     return int(np.dot(weights, inputs) >= 0)
+
+def NOT(u):
+    weights = np.array([-1.5, 1])
+    inputs = np.array([u, 1])
+    return step_function(weights, inputs)
 
 def AND(u1, u2):
     weights = np.array([1, 1, -1.25])
     inputs = np.array([u1, u2, 1])
-    return int(np.dot(weights, inputs) >= 0)
+    return step_function(weights, inputs)
 
 def NAND(u1, u2):
     weights = np.array([-1, -1, 1.25])
     inputs = np.array([u1, u2, 1])
-    return int(np.dot(weights, inputs) >= 0)
+    return step_function(weights, inputs)
 
 def OR(u1, u2):
     weights = np.array([1.5, 1.5, -1])
     inputs = np.array([u1, u2, 1])
-    return int(np.dot(weights, inputs) >= 0)
+    return step_function(weights, inputs)
 
 print("NOT:")
 for u1 in [0, 1]:
